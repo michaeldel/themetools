7c6ad75058046990b7bf96033cc81c8c1dbce62c
michaeldel <michaeldel@protonmail.com>
2020-06-27 06:11:17 +0200

    Define VSCode theme dataclass

--- themetools/vscode.py
+++ themetools/vscode.py
@@ -1,37 +1,35 @@
+from __future__ import annotations
+
+import enum
 import sys
 
-import json5 as json
+from dataclasses import dataclass
+from typing import List, Optional
 
 
-def load_theme(filename: str):
-    with open(filename, 'r') as f:
-        theme = json.load(f)
+@dataclass(frozen=True)
+class VSCodeTheme:
+    name: str
+    parent: Optional[VSCodeTheme]
+    spec: List[Scope]
 
-    assert theme['$schema'] == 'vscode://schemas/color-theme'
-    return theme
+
+@dataclass(frozen=True)
+class Scope:
+    name: Optional[str]
+    paths: List[str]
+    settings: Settings
+
+
+@dataclass(frozen=True)
+class Settings:
+    font_style: Optional[FontStyle] = None
+    foreground: Optional[str] = None
+
+
+class FontStyle(enum.Enum):
+    BOLD = 'bold'
+    UNDERLINE = 'underline'
 
 
 assert len(sys.argv) == 2
-theme = load_theme(sys.argv[1])
-print(f"Theme: {theme['name']}")
-
-scopes = {}
-
-for tc in theme['tokenColors']:
-    settings = tc['settings']
-    scope = tc['scope']
-
-    if isinstance(scope, str):
-        scopes[scope] = settings
-    elif isinstance(scope, list):
-        for s in scope:
-            scopes[s] = settings
-    else:
-        raise
-
-for scope, settings in scopes.items():
-    hexcode = settings['foreground'].lstrip('#').upper()
-    r, g, b = tuple(int(hexcode[i : i + 2], 16) for i in (0, 2, 4))
-    formatted = f"\033[38;2;{r};{g};{b}m#{hexcode.lower()}\033[0m"
-
-    print(scope, formatted)
